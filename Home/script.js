63b264d24ca3f4290a0ba7ff4464d5c67dbae97d
Aditya kumar <150524610+theAdityah@users.noreply.github.com>
2024-07-25 13:23:17 +0530

    Delete Home directory

--- Home/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('playButton').addEventListener('click', function() {
-            document.getElementById('popup').style.display = 'block';
-        });
-
-        window.addEventListener('click', function(event) {
-            if (event.target === document.getElementById('popup')) {
-                document.getElementById('popup').style.display = 'none';
-            }
-        });
