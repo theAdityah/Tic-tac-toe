bc6fc425d542539c91e09719b9e97d82b82e00f5
Aditya kumar <150524610+theAdityah@users.noreply.github.com>
2024-08-01 17:22:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-document.getElementById('playButton').addEventListener('click', function() {
+        document.getElementById('play').addEventListener('click', function() {
             document.getElementById('popup').style.display = 'block';
+            document.getElementById('overlay').style.display = 'block'; // Show overlay
         });
 
         window.addEventListener('click', function(event) {
-            if (event.target === document.getElementById('popup')) {
+            if (event.target === document.getElementById('overlay')) {
                 document.getElementById('popup').style.display = 'none';
+                document.getElementById('overlay').style.display = 'none'; // Hide overlay
             }
         });
